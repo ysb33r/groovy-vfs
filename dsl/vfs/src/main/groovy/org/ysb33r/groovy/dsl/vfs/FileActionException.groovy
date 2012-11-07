1dc755dad0ab7b3901482751e174435cb6e1a89a
Schalk W. Cronje <yw-github@af.org.za>
2012-11-07 22:45:26 +0000

    Copy operation curretnly does not support filters or copying where source is a directory. ONly the low-level functionality is implemented. TH eDSL level cp needs to be updated to call the lower-level correctly.

--- dsl/vfs/src/main/groovy/org/ysb33r/groovy/dsl/vfs/FileActionException.groovy
+++ dsl/vfs/src/main/groovy/org/ysb33r/groovy/dsl/vfs/FileActionException.groovy
@@ -3,7 +3,8 @@
 //
 // This software is licensed under the Apche License 2.0
 // See http://www.apache.org/licenses/LICENSE-2.0 for license details
-// ============================================================================package org.ysb33r.groovy.dsl.vfs
+// ============================================================================
+package org.ysb33r.groovy.dsl.vfs
 
 import org.apache.commons.vfs2.FileSystemException
 
