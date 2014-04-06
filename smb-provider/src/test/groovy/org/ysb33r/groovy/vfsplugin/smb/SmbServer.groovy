7672dcd59004559e46edffbe96dafc2a48597a37
Schalk W. Cronjé <ysb33r@gmail.com>
2014-04-06 12:11:15 +0100

    Fixed the issue where smb-provider unittests were failing if run from the top of the project

--- smb-provider/src/test/groovy/org/ysb33r/groovy/vfsplugin/smb/SmbServer.groovy
+++ smb-provider/src/test/groovy/org/ysb33r/groovy/vfsplugin/smb/SmbServer.groovy
@@ -15,6 +15,7 @@ import org.alfresco.jlan.app.JLANCifsServer
 class SmbServer implements Runnable {
 
     static final String CONFIGXML = System.getProperty('JLANCONFIG') ?: 'src/test/resources/jlanserver.xml'
+    static final String ROOT = System.getProperty('ROOT') ?: '.'
     static final def CONFIG = new XmlSlurper().parse(new File(CONFIGXML))
     static final String HOSTNAME = CONFIG.SMB.host.bindto.text()
     static final String PORT = CONFIG.SMB.host.tcpipSMB.@port
@@ -22,7 +23,7 @@ class SmbServer implements Runnable {
     static final String USER = CONFIG.security.users.user[1].@name
     static final String PASSWORD = CONFIG.security.users.user[1].password.text()
     static final String READSHARE = CONFIG.shares.diskshare[0].@name
-    static final String READDIR = new File(CONFIG.shares.diskshare[0].driver.LocalPath.text())
+    static final String READDIR = new File(CONFIG.shares.diskshare[0].driver.LocalPath.text()).absoluteFile
     static final String WRITESHARE = CONFIG.shares.diskshare[1].@name
 
     def server
