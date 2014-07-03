9c2248ef0260cbc770030b62f8c7874744b59b5f
Schalk W. Cronjé <ysb33r@gmail.com>
2014-07-03 10:52:37 +0100

    More updates for S3 support

--- cloud-provider-core/src/main/groovy/org/ysb33r/groovy/vfsplugin/cloud/core/AbstractCloudFileProvider.groovy
+++ cloud-provider-core/src/main/groovy/org/ysb33r/groovy/vfsplugin/cloud/core/AbstractCloudFileProvider.groovy
@@ -8,7 +8,6 @@
 // distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 //
-
 // ============================================================================
 package org.ysb33r.groovy.vfsplugin.cloud.core
 
--- cloud-provider-core/src/main/groovy/org/ysb33r/groovy/vfsplugin/cloud/s3/S3FileSystemConfigBuilder.groovy
+++ cloud-provider-core/src/main/groovy/org/ysb33r/groovy/vfsplugin/cloud/s3/S3FileSystemConfigBuilder.groovy
@@ -1,3 +1,14 @@
+// ============================================================================
+// (C) Copyright Schalk W. Cronje 2014
+//
+// This software is licensed under the Apache License 2.0
+// See http://www.apache.org/licenses/LICENSE-2.0 for license details
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
+//
+// ============================================================================
 package org.ysb33r.groovy.vfsplugin.cloud.s3
 
 import groovy.transform.CompileStatic
@@ -18,3 +29,6 @@ class S3FileSystemConfigBuilder extends AbstractCloudFileSystemConfigBuilder {
     protected Class<? extends FileSystem> getConfigClass() { S3FileSystem.class }
 
 }
+
+// TODO: Add specific S3 config
+// TODO: Add ACL operations
