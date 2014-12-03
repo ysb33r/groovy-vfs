cdc25f436a3dc667e6bbe3685a6cf1d86fb3c74a
Schalk W. Cronjé <ysb33r@gmail.com>
2014-12-03 23:12:35 +0000

    Correct license text for SMB provider as it is LGPL rather than Apache

--- smb-provider/LICENSE.md
+++
@@ -1,5 +0,0 @@
-= License
-
-It is still unclear under which license the smb-provider will be released. There is a good chance that unlike the rest of 
-VFS, which is Apache 2.0, the smb-provider will be LPGL due to its dependency on jCFIS
-
--- smb-provider/src/test/groovy/org/ysb33r/groovy/vfsplugin/smb/SmbFileNameSpec.groovy
+++ smb-provider/src/test/groovy/org/ysb33r/groovy/vfsplugin/smb/SmbFileNameSpec.groovy
@@ -1,14 +1,21 @@
 // ============================================================================
 // (C) Copyright Schalk W. Cronje 2014
 //
-// This software is licensed under the Apache License 2.0
-// See http://www.apache.org/licenses/LICENSE-2.0 for license details
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
 //
-// Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and limitations under the License.
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
 //
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 // ============================================================================
+
 package org.ysb33r.groovy.vfsplugin.smb
 
 import spock.lang.*
