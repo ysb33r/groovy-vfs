4d5e5b68958c823beb55b5b1b4914251f45a1dac
Schalk W. Cronjé <ysb33r@gmail.com>
2015-01-05 21:41:41 +0000

    Yet more license updates. Grrr....

--- cmdline/src/test/resources/create-cat-files.sh
+++ cmdline/src/test/resources/create-cat-files.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
+#
 # ============================================================================
-# (C) Copyright Schalk W. Cronje 2014
+# (C) Copyright Schalk W. Cronje 2013-2015
 #
 # This software is licensed under the Apache License 2.0
-# See http:#www.apache.org/licenses/LICENSE-2.0 for license details
+# See http://www.apache.org/licenses/LICENSE-2.0 for license details
 #
 # Unless required by applicable law or agreed to in writing, software distributed under the License is
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 #
 # ============================================================================
-# Helper script to generate expected output for 'vfs cat' operations
-# Known to work on Mac (BSD cat) and Linux (GNU cat)
-# ============================================================================
+#
 
 srcd="$(dirname $0)/test-files"
 destd="${srcd}/cat-expected"
