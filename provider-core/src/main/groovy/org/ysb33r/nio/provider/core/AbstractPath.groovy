3e50b7c1a75a0e5a5b75f8f5581b9be5a3715c31
Schalk W. Cronjé <ysb33r@gmail.com>
2016-06-23 13:38:35 -0700

    Added friendlyURI()

--- provider-core/src/main/groovy/org/ysb33r/nio/provider/core/AbstractPath.groovy
+++ provider-core/src/main/groovy/org/ysb33r/nio/provider/core/AbstractPath.groovy
@@ -1,14 +1,35 @@
+/*
+ * ============================================================================
+ * (C) Copyright Schalk W. Cronje 2013-2016
+ *
+ * This software is licensed under the Apache License 2.0
+ * See http://www.apache.org/licenses/LICENSE-2.0 for license details
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ * ============================================================================
+ */
 package org.ysb33r.nio.provider.core
 
 import groovy.transform.CompileStatic
 
 import java.nio.file.Path
 
-/**
- * @author Schalk W. Cronjé
+/** A base class for creating custom Path implementations.
+ *
  */
 @CompileStatic
 abstract class AbstractPath implements Path {
+
+    /** Masks out password when printing URI
+     *
+     * @return A string where the password has been replaced by a series of asterisks.
+     */
+    String friendlyURI() {
+        UriUtils.friendlyURI(toUri())
+    }
 
     /** Returns the character that is used to sperate path components.
      *
