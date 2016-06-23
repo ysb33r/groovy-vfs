abf9a7428a2a90941c0a085787e20e3aa3add2e2
Schalk W. Cronjé <ysb33r@gmail.com>
2016-06-23 11:37:45 -0700

    Added license headers

--- test-servers/smb/src/main/groovy/org/ysb33r/vfs/test/services/SmbServer.groovy
+++ test-servers/smb/src/main/groovy/org/ysb33r/vfs/test/services/SmbServer.groovy
@@ -1,6 +1,6 @@
 /*
  * ============================================================================
- * (C) Copyright Schalk W. Cronje 2013-2015
+ * (C) Copyright Schalk W. Cronje 2013-2016
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -18,7 +18,6 @@
  *
  * ============================================================================
  */
-
 package org.ysb33r.vfs.test.services
 
 import groovy.transform.CompileStatic
