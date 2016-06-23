c3c3006c5ff2b90f4c2393e8a1f465a7cf6946aa
Schalk W. Cronjé <ysb33r@gmail.com>
2016-06-23 11:41:29 -0700

    Modified scheme property to accessors

--- provider-core/src/main/groovy/org/ysb33r/nio/provider/core/AbstractFileSystemProvider.groovy
+++ provider-core/src/main/groovy/org/ysb33r/nio/provider/core/AbstractFileSystemProvider.groovy
@@ -1,3 +1,16 @@
+/*
+ * ============================================================================
+ * (C) Copyright Schalk W. Cronje 2013-2016
+ *
+ * This software is licensed under the Apache License 2.0
+ * See http://www.apache.org/licenses/LICENSE-2.0 for license details
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ * ============================================================================
+ */
 package org.ysb33r.nio.provider.core
 
 import groovy.transform.CompileStatic
@@ -12,9 +25,10 @@ import java.nio.file.spi.FileSystemProvider
 @CompileStatic
 abstract class AbstractFileSystemProvider extends FileSystemProvider {
 
-    final String scheme
+    String getScheme() {this.scheme}
 
     AbstractFileSystemProvider(final String scheme) {
+        super()
         this.scheme = System.getProperty(getSchemeSystemPropertyName(scheme)) ?: scheme
     }
 
@@ -49,4 +63,6 @@ abstract class AbstractFileSystemProvider extends FileSystemProvider {
     protected String getSchemeSystemPropertyName(final String scheme) {
         "org.ysb33r.nio.provider.${scheme}.scheme"
     }
+
+    private final String scheme
 }
