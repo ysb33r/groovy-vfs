21df189323f615948d3f06ea018bee104720af85
Schalk W. Cronjé <ysb33r@gmail.com>
2017-11-12 19:15:30 +0100

    WIP: Update license headers

--- core/src/test/groovy/org/ysb33r/vfs/auth/CredentialsSpec.groovy
+++ core/src/test/groovy/org/ysb33r/vfs/auth/CredentialsSpec.groovy
@@ -1,3 +1,16 @@
+/*
+ * ============================================================================
+ * (C) Copyright Schalk W. Cronje 2013-2017
+ *
+ * This software is licensed under the Apache License 2.0
+ * See http://www.apache.org/licenses/LICENSE-2.0 for license details
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ * ============================================================================
+ */
 package org.ysb33r.vfs.auth
 
 import spock.lang.Specification
@@ -12,7 +25,7 @@ class CredentialsSpec extends Specification {
 
         expect:
         creds.username == 'foo'
-        creds.password == 'bar'
+        creds.password.toString() == 'bar'
     }
 
     def "Create basic authentication supplier with username only"() {
